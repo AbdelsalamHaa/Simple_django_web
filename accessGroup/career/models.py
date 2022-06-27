9b754f306efe7ca9602c559a09fa0fdefab8de2e
AbdelsalamHaa <abdelsalam.h.a.a@gmail.com>
2022-06-27 10:50:21 +0800

    removing the bio from the model and testing to switch branches

--- accessGroup/career/models.py
+++ accessGroup/career/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
+    # bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length = 20)
